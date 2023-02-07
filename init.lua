d68b8dd7eea907cc370645256f7598708a6744a1
mrthere3 <871780968@qq.com>
2023-02-07 15:40:40 +0800

    cocloresheme

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ require('packinit') -- 插件配置
 require('keymap')   -- 按键配置
 local G = require('G')
 -- G.cmd("colorscheme oceanic_material")
--- G.cmd([[colorscheme habamax]])
+G.cmd([[colorscheme habamax]])
 G.g.python3_host_prog = '/usr/bin/python3.8'
 G.g.python_host_prog = '/usr/bin/python3.8'
 G.g.loaded_node_provider = '/usr/bin/node'
